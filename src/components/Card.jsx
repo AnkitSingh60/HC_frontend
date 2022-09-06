7df442a6caf0017785f7d491afa25cf52ddf366b
AnkitSingh60 <ankitanwar730@gmail.com>
2022-09-06 16:52:01 +0530

    card mapped

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,23 @@ const Card = ({ post }) => {
     // console.log('post:', post)
     return (
         <div className='mainContainer'>
-            <div className="card" style={{width: "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
+        {
+            post.map((item)=>{
+                return(
+                    
+                    <div className="card" key={item.id}>
+        
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">{item.title.slice(0, 30)}</h5>
+                    <p className="card-text">{item.body.slice(0, 60)}</p>
+                    <a href="#" className="btn btn-primary">View</a>
                 </div>
             </div>
+                )
+            })
+        }
+
+            
         </div>
     )
 }
