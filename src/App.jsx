57ab043c0faf551f59d42068ec700d327a666990
AnkitSingh60 <ankitanwar730@gmail.com>
2022-09-06 16:30:05 +0530

    data fetched sucessfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
-  
+  const [post, setPost] = useState([])
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      // console.log('data:', data)
+      setPost(data);
+    } catch (error) {
+      console.log('error:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-        <h1>hey kiddo</h1>
+      <Card post={post} />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Card = ({post}) => {
+  console.log('post:', post)
+  return (
+    <div>Card</div>
+  )
+}
+
+export default Card
